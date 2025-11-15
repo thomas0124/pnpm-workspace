cb68b3fba1cf0bc099aeeafdb54f10aca83087f3
thomas0124 <qingshuitouzhen@gmail.com>
2025-11-16 00:58:30 +0900

    setup: ⚙️ frontendディレクトリにリネーム

---
+++ packages/frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { message } from '@uttk/backend'
+
+export default function Home() {
+  return (
+    <main style={{display:'flex',height:'100vh',alignItems:'center',justifyContent:'center',fontFamily:'sans-serif'}}>
+      <div>
+        <h1>Message shared from backend (Hono)</h1>
+        <p style={{fontSize:20,marginTop:8}}>{message}</p>
+      </div>
+    </main>
+  )
+}
