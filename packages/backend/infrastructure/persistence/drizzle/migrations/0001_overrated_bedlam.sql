52a64f6cc472ce121b685e8203012490126f399b
sekibuuun <sekibun3109@gmail.com>
2025-12-01 01:57:07 +0900

    feat: add initial migration for exhibition and exhibitor tables, including foreign key references and unique constraints

---
+++ packages/backend/infrastructure/persistence/drizzle/migrations/0001_overrated_bedlam.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `exhibition_ar_design` (
+	`id` text PRIMARY KEY NOT NULL,
+	`url` text
+);
+--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_exhibitor` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`password_hash` text NOT NULL,
+	`created_at` text NOT NULL,
+	`updated_at` text NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_exhibitor`("id", "name", "password_hash", "created_at", "updated_at") SELECT "id", "name", "password_hash", "created_at", "updated_at" FROM `exhibitor`;--> statement-breakpoint
+DROP TABLE `exhibitor`;--> statement-breakpoint
+ALTER TABLE `__new_exhibitor` RENAME TO `exhibitor`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `exhibitor_name_unique` ON `exhibitor` (`name`);--> statement-breakpoint
+ALTER TABLE `exhibition_information` ADD `exhibition_ar_design_id` text REFERENCES exhibition_ar_design(id);--> statement-breakpoint
+ALTER TABLE `exhibition_information` ADD `exhibitor_name` text NOT NULL;
