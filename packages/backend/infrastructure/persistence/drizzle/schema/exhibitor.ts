8bc54238b047794b623cd86851e4850db7fe2ed7
sekibuuun <sekibun3109@gmail.com>
2025-11-30 16:58:39 +0900

    fix: `createdAt`と`updatedAt`を`text`に変更

--- packages/backend/infrastructure/persistence/drizzle/schema/exhibitor.ts
+++ packages/backend/infrastructure/persistence/drizzle/schema/exhibitor.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 /**
  * 出展者アカウントテーブル
@@ -7,8 +7,8 @@ export const exhibitor = sqliteTable('exhibitor', {
   id: text('id').primaryKey(),
   name: text('name').notNull(),
   passwordHash: text('password_hash').notNull(),
-  createdAt: integer('created_at').notNull(),
-  updatedAt: integer('updated_at').notNull(),
+  createdAt: text('created_at').notNull(),
+  updatedAt: text('updated_at').notNull(),
 })
 
 export type Exhibitor = typeof exhibitor.$inferSelect
