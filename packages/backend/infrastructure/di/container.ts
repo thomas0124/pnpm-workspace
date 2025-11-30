11c090dba89468e32ea26a6add31ced1b6e4add1
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-11-30 23:54:44 +0900

    refactor: createExhibitorRepositoryを使用してExhibitorRepositoryの構造を改善

--- packages/backend/infrastructure/di/container.ts
+++ packages/backend/infrastructure/di/container.ts
@@ -2,14 +2,8 @@ import type { ExhibitorRepository } from '../../domain/repositories/exhibitorRep
 import * as drizzleExhibitorRepo from '../persistence/drizzle/ExhibitorRepository'
 
 export const createContainer = (d1: D1Database) => {
-  const exhibitorRepository: ExhibitorRepository = {
-    save: (exhibitor) => drizzleExhibitorRepo.save(exhibitor, d1),
-    findById: (id) => drizzleExhibitorRepo.findById(id, d1),
-    findByName: (name) => drizzleExhibitorRepo.findByName(name, d1),
-    existsByName: (name) => drizzleExhibitorRepo.existsByName(name, d1),
-    delete: (id) => drizzleExhibitorRepo.deleteExhibitor(id, d1),
-    findAll: () => drizzleExhibitorRepo.findAll(d1),
-  }
+    const exhibitorRepository: ExhibitorRepository = 
+    drizzleExhibitorRepo.createExhibitorRepository(d1)
 
   return {
     exhibitorRepository,
