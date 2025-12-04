1de046c34c44f53b88908183dafebd7d7a29be5a
sekibuuun <sekibun3109@gmail.com>
2025-12-05 00:29:17 +0900

    refactor: ルート定義をメソッドチェーンを用いる形に修正

--- packages/backend/presentation/routes/exhibition/exhibition.ts
+++ packages/backend/presentation/routes/exhibition/exhibition.ts
@@ -6,16 +6,13 @@ import { exhibitionCoreRoutes } from './exhibitionCore'
 import { exhibitionImageRoutes } from './exhibitionImage'
 import { exhibitionInformationRoutes } from './exhibitionInformation'
 
+// GitHub issue #3148 の解決策: メソッドチェーン形式で定義することで型推論が正しく動作する
 export const exhibitionRoutes = new Hono<{ Bindings: Bindings }>()
-
-// すべてのルートに認証ミドルウェアを適用
-exhibitionRoutes.use('*', authMiddleware)
-
-// 基本CRUDと状態変更のルート
-exhibitionRoutes.route('/', exhibitionCoreRoutes)
-
-// 基本情報更新のルート
-exhibitionRoutes.route('/:exhibitionId/information', exhibitionInformationRoutes)
-
-// 画像関連のルート
-exhibitionRoutes.route('/:exhibitionId/information/image', exhibitionImageRoutes)
+  // すべてのルートに認証ミドルウェアを適用
+  .use('*', authMiddleware)
+  // 基本CRUDと状態変更のルート
+  .route('/', exhibitionCoreRoutes)
+  // 基本情報更新のルート
+  .route('/:exhibitionId/information', exhibitionInformationRoutes)
+  // 画像関連のルート
+  .route('/:exhibitionId/information/image', exhibitionImageRoutes)
--- packages/backend/presentation/routes/exhibitionArDesign.ts
+++ packages/backend/presentation/routes/exhibitionArDesign.ts
@@ -4,7 +4,7 @@ import { handleListArDesign } from '../handlers/exhibitionArDesign'
 import type { Bindings } from '../handlers/index'
 import { authMiddleware } from '../middlewares/authMiddleware'
 
+// GitHub issue #3148 の解決策: メソッドチェーン形式で定義することで型推論が正しく動作する
 export const arDesignRoutes = new Hono<{ Bindings: Bindings }>()
-
-// ARデザイン一覧取得（認証必須）
-arDesignRoutes.get('/', authMiddleware, ...handleListArDesign)
+  // ARデザイン一覧取得（認証必須）
+  .get('/', authMiddleware, ...handleListArDesign)
