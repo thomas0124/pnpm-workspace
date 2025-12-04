cede7f4fa16ca398d9ed0c79b7e064b8a695ff42
sekibuuun <sekibun3109@gmail.com>
2025-12-05 01:22:40 +0900

    chore: `pnpm fix`

--- packages/frontend/src/app/login/page.tsx
+++ packages/frontend/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 import { LoginFormWrapper } from "@/components/login/loginFormWrapper";
 
--- packages/frontend/src/app/register/page.tsx
+++ packages/frontend/src/app/register/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 import { RegisterFormWrapper } from "@/components/register/registerFormWrapper";
 
