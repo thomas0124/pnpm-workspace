8a7b1169bf9b728c2af557a3d832d2acacafd2eb
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-12-05 17:18:31 +0900

    fix: 出展画像関連のインポートを削除

--- packages/backend/presentation/routes/exhibition/exhibition.ts
+++ packages/backend/presentation/routes/exhibition/exhibition.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono'
-
 import type { Bindings } from '../../handlers/index'
 import { authMiddleware } from '../../middlewares/authMiddleware'
 import { exhibitionCoreRoutes } from './exhibitionCore'
-import { exhibitionImageRoutes } from './exhibitionImage'
 import { exhibitionInformationRoutes } from './exhibitionInformation'
 
 // GitHub issue #3148 の解決策: メソッドチェーン形式で定義することで型推論が正しく動作する
